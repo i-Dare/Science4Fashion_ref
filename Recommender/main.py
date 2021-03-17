83ff7e252711a21aed0fe0cd51e62b4b5f7094dc
Sotiris Tsarouchis <sotirisftsar@gmail.com>
2021-03-17 09:31:16 +0200

    update main

--- Recommender/main.py
+++ Recommender/main.py
@@ -1,21 +1,21 @@
 import pandas as pd
 from evaluator import evaluator
 from ContentBasedRecom import cb_rf
-import helper_functions
-
+from core.helper_functions import Helper
 eval = evaluator()
-currendDir = helper_functions.WEB_CRAWLERS
-engine = helper_functions.ENGINE
-dbName = helper_functions.DB_NAME
+currendDir = Helper.WEB_CRAWLERS
+engine = Helper.ENGINE
+dbName = Helper.DB_NAME
 
 df = pd.DataFrame(columns=['user','metric','score'])
 df2 = pd.DataFrame(columns=['user','metric','score'])
-for u in range(10):
-    model = cb_rf(input_user=u, engine=engine)
-    grade_df = model.split_and_predict()
-    a,b = eval.user_metric_df(user=u,df=grade_df,threshold=3)
-    df = df.append(a)
-    df2 = df2.append(b)
+u = 0
+# for u in range(10):
+model = cb_rf(input_user=u, engine=engine)
+grade_df = model.split_and_predict()
+a,b = eval.user_metric_df(user=u,df=grade_df,threshold=3)
+df = df.append(a)
+df2 = df2.append(b)
 
 eval.visualize_bars(df = df,axe_x='metric',axe_y='score',hue='user')
 
