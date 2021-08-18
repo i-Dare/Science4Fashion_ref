c49baaa687dafbca16defbfa7869b8d3d54a1328
Argirios Vartholomeos <arvartho@gmail.com>
2021-08-18 09:58:22 +0300

    Changes in db_init script
    * Initialize DB with lower cased values.
    * Remove Shutterstock from Adapters

--- Database/db_init_script.py
+++ Database/db_init_script.py
@@ -15,10 +15,10 @@ if __name__ == '__main__':
       table_df =  pd.read_sql_query("SELECT * FROM %s.dbo.%s" % (config.DB_NAME, table), config.ENGINE)
       file_df = pd.read_csv(os.path.join(config.RESOURCESDIR, 'db_init', '%s.csv' % table))
       for value in file_df['Description'].unique():
-         if value not in table_df['Description'].unique():
-            print('Insert value %s in table %s' % (value, table))
+         if value not in table_df['Description'].str.lower().unique():
+            print('Insert value %s in table %s' % (value.lower(), table))
             query = '''INSERT INTO %s.dbo.%s (Description)
-                        VALUES ('%s');''' % (config.DB_NAME, table, value)
+                        VALUES ('%s');''' % (config.DB_NAME, table, value.lower())
             with config.ENGINE.begin() as conn:
                conn.execute(query)
       
